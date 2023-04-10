2b4cb87f0fb3af79840670b2f901c8112477375b
waltersuarez20 <127705995+waltersuarez20@users.noreply.github.com>
2023-04-09 22:57:33 -0500

    Add files via upload

---
+++ taller js/programa.js
@@ -0,0 +1,24 @@
+let a= 30;
+let b= 46;
+
+if (a<b){
+    console.log ("La variable con menor valor es a:"+ a);
+}
+    else {("La variable con menor valor es b:"+ b);
+} 
+
+
+// 2. Definir tres variables, asignarles valores y utilizar decisiones para imprimir
+// en la que tenga el mayor valor
+
+let c = 19;
+let d = 14;
+let e = 22;
+
+if (c>d && c>e) {
+    console.log ("La variable con menor valor es c:" + c);
+} else if (d>c && d>e) {
+    console.log("La variable con menor valor es d:" +  d);
+} else {
+    console.log ("La variBLE CON MAYOR VALOR ES e:" + e);
+}
